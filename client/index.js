76534aec30605f7863f048405a0d8f546cca5cc2
tianhuofenglin <tianhuofenglin@qq.com>
2016-05-19 10:59:45 +0800

    服务端渲染，代码重构中

--- client/index.js
+++ client/index.js
@@ -5,13 +5,13 @@ import { Provider } from 'react-redux'
 import configureStore from '../app/store/configureStore'
 import App from '../app/containers/App'
 
-const initialState = window.__INITIAL_STATE__
-const store = configureStore(initialState)
-const rootElement = document.getElementById('maple')
+const initialState = window.__INITIAL_STATE__;
+const store = configureStore(initialState);
+const rootElement = document.getElementById('maple');
 
 render(
   <Provider store={store}>
     <App/>
   </Provider>,
   rootElement
-)
+);
